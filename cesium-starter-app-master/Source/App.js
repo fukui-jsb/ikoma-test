359cf407a177b1aa4643ec3fae69a2c21d2440a2
fukui-jsb <doumoto.hawks7@gmail.com>
2017-01-14 16:22:47 +0900

    Update App.js

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,3 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://fukui-jsb.github.io/ikoma-test/cesium-starter-app-master/index.html'));
